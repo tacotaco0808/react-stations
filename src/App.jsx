deea5acd5c41994d6c1062ca8d9449fc7833d35b
takutou <takuto1488@gmail.com>
2023-10-24 15:48:12 +0900

    7まで終わらせました

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/pomeranian/n02112018_514.jpg',
+  )
+
+  async function dogUrlSetter() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    console.log(response)
+    const data = await response.json()
+    console.log(data.message)
+    setDogUrl(data.message)
+  }
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header className="head">いぬころ</header>
+      <div>
+        <p>犬の画像を表示するよ!</p>
+        <img className="dog_image" src={dogUrl} alt="superdog" />
+      </div>
+
+      <hr />
+      <button onClick={dogUrlSetter}>更新</button>
     </div>
   )
 }
